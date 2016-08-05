73122a5c9aef42f64ec377306d3ad5ad67e5253e
Alan Knowles <alan@roojs.com>
2016-08-05 17:29:51 +0800

    PDO/DataObject.php

--- PDO/DataObject.php
+++ PDO/DataObject.php
@@ -474,6 +474,10 @@ class DB_DataObject
     function quoteIdentifier($str)
     {
         $pdo = $this->_PDO();
+        if (!is_a($pdo, self::$config['PDO'])) {
+            $this->raiseError("Can not quoteIdentifier as connection failed", $pdo, self::ERROR_DIE);
+        }
+        
         switch($pdo->getAttribute(PDO::ATTR_DRIVER_NAME)) {
             case 'mysql':
                 return '`' . str_replace('`', '``', $str) . '`';
