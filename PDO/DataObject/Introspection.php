7453c55bf8571f78d83f7b6142bfe1fab998b873
Alan Knowles <alan@roojs.com>
2016-08-08 15:04:58 +0800

    PDO/DataObject/Introspection.php

---
+++ PDO/DataObject/Introspection.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Object Based Database Query Builder and data store
+ *  - Introspection Component.
+ *
+ * For PHP versions  5 and 7
+ * 
+ * 
+ * Copyright (c) 2015 Alan Knowles
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU Lesser General Public License as   
+ * published by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * Lesser General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *  
+ * @category   Database
+ * @package    PDO_DataObject
+ * @author     Alan Knowles <alan@roojs.com>
+ * @copyright  2016 Alan Knowles
+ * @license    https://www.gnu.org/licenses/lgpl-3.0.en.html  LGPL 3
+ * @version    1.0
+ * @link       https://github.com/roojs/PDO_DataObject
+ */
+  
+  
+class PDO_DataObject_Introspection
+{
+    
+    private $dataobject;
+    
+    function __construct($do)
+    {
+        $this->dataobject = $do;
+    }
+    
+    
+    function databaseStructure()
+    {
+        
+    }
+    
+    
+    
+}
