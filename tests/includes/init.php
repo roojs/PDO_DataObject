31434fd0620d231234c31c42674b00551d1444d7
Alan Knowles <alan@roojs.com>
2016-08-09 13:53:39 +0800

    tests/includes/init.php

--- tests/includes/init.php
+++ tests/includes/init.php
@@ -1,12 +1,13 @@
 <?php
 
-require_once __DIR__ .'/../../PDO/DataObject.php';
+ini_set('include_path', __DIR__ .'/../../' . PATH_SEPARATOR. ini_get('include_path');
+require_once 'PDO/DataObject.php';
 require_once __DIR__ .'/PDO_Dummy.php';
 
 PDO_DataObject::loadConfig(array(
     'database' => 'mysql://user:pass@localhost/testdb',
     'PDO' => 'PDO_Dummy',
-    'debug' => 1,
+    'debug' => 5,
     
 ));
 
