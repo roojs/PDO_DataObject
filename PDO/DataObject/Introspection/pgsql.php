77dd138bcae2fa4ac10de52017c67ea12e8e94a4
Alan Knowles <alan@roojs.com>
2016-08-10 13:52:49 +0800

    PDO/DataObject/Introspection/pgsql.php

--- PDO/DataObject/Introspection/pgsql.php
+++ PDO/DataObject/Introspection/pgsql.php
@@ -39,15 +39,51 @@ class PDO_DataObject_Introspection_pgsql extends PDO_DataObject_Introspection
     {
         switch ($type) {
             case 'tables':
-                return 'SHOW TABLES';
+                return 'SELECT c.relname AS "Name"'
+                        . ' FROM pg_class c, pg_user u'
+                        . ' WHERE c.relowner = u.usesysid'
+                        . " AND c.relkind = 'r'"
+                        . ' AND NOT EXISTS'
+                        . ' (SELECT 1 FROM pg_views'
+                        . '  WHERE viewname = c.relname)'
+                        . " AND c.relname !~ '^(pg_|sql_)'"
+                        . ' UNION'
+                        . ' SELECT c.relname AS "Name"'
+                        . ' FROM pg_class c'
+                        . " WHERE c.relkind = 'r'"
+                        . ' AND NOT EXISTS'
+                        . ' (SELECT 1 FROM pg_views'
+                        . '  WHERE viewname = c.relname)'
+                        . ' AND NOT EXISTS'
+                        . ' (SELECT 1 FROM pg_user'
+                        . '  WHERE usesysid = c.relowner)'
+                        . " AND c.relname !~ '^pg_'";
+            case 'schema.tables':
+                return "SELECT schemaname || '.' || tablename"
+                        . ' AS "Name"'
+                        . ' FROM pg_catalog.pg_tables'
+                        . ' WHERE schemaname NOT IN'
+                        . " ('pg_catalog', 'information_schema', 'pg_toast')";
+            case 'schema.views':
+                return "SELECT schemaname || '.' || viewname from pg_views WHERE schemaname"
+                        . " NOT IN ('information_schema', 'pg_catalog')";
+            case 'views':
+                // Table cols: viewname | viewowner | definition
+                return 'SELECT viewname from pg_views WHERE schemaname'
+                        . " NOT IN ('information_schema', 'pg_catalog')";
+            case 'users':
+                // cols: usename |usesysid|usecreatedb|usetrace|usesuper|usecatupd|passwd  |valuntil
+                return 'SELECT usename FROM pg_user';
             case 'databases':
-                return 'SHOW DATABASES';
+                return 'SELECT datname FROM pg_database';
+            case 'functions':
+            case 'procedures':
+                return 'SELECT proname FROM pg_proc WHERE proowner <> 1';
             default:
                 return null;
         }
     }
 
-    // }}}
     
     
     /**
@@ -67,10 +103,51 @@ class PDO_DataObject_Introspection_pgsql extends PDO_DataObject_Introspection
      */
     function tableInfo($string)
     {
-         
+        
+        $database = $this->do->PDO()->database;
         
         $records =  $this->do
-            ->query("DESCRIBE $string")
+            ->query("SELECT  
+    f.attnum AS number,  
+    f.attname AS name,  
+    f.attnum,  
+    f.attnotnull AS notnull,  
+    pg_catalog.format_type(f.atttypid,f.atttypmod) AS type,  
+    CASE  
+        WHEN p.contype = 'p' THEN 't'  
+        ELSE 'f'  
+    END AS primarykey,  
+    CASE  
+        WHEN p.contype = 'u' THEN 't'  
+        ELSE 'f'
+    END AS uniquekey,
+    CASE
+        WHEN p.contype = 'f' THEN g.relname
+    END AS foreignkey,
+    CASE
+        WHEN p.contype = 'f' THEN p.confkey
+    END AS foreignkey_fieldnum,
+    CASE
+        WHEN p.contype = 'f' THEN g.relname
+    END AS foreignkey,
+    CASE
+        WHEN p.contype = 'f' THEN p.conkey
+    END AS foreignkey_connnum,
+    CASE
+        WHEN f.atthasdef = 't' THEN d.adsrc
+    END AS default
+FROM pg_attribute f  
+    JOIN pg_class c ON c.oid = f.attrelid  
+    JOIN pg_type t ON t.oid = f.atttypid  
+    LEFT JOIN pg_attrdef d ON d.adrelid = c.oid AND d.adnum = f.attnum  
+    LEFT JOIN pg_namespace n ON n.oid = c.relnamespace  
+    LEFT JOIN pg_constraint p ON p.conrelid = c.oid AND f.attnum = ANY (p.conkey)  
+    LEFT JOIN pg_class AS g ON p.confrelid = g.oid  
+WHERE c.relkind = 'r'::char  
+    AND n.nspname = '%s'  -- Replace with Schema name  
+    AND c.relname = '%s'  -- Replace with table name  
+    AND f.attnum > 0 ORDER BY number
+    ")
             ->fetchAll(false,false,'toArray');
         
         
