d2f0cd0a544dffdf49ce7911edac8e4d5cf2144b
Alan Knowles <alan@roojs.com>
2016-08-10 17:57:02 +0800

    tests/includes/sqlite_simple_test.php tests/includesEssentialSQL.db

---
+++ tests/includes/sqlite_simple_test.php
@@ -0,0 +1,8 @@
+<?php
+
+$pdo = new PDO("sqlite:".__DIR__.'EssentialSQL.db');
+$res = $pdo->query("SELECT name FROM sqlite_master WHERE type='table' "
+                       . 'UNION ALL SELECT name FROM sqlite_temp_master '
+                       . "WHERE type='table' ORDER BY name");
+
+print_R($res->fetchAll());
