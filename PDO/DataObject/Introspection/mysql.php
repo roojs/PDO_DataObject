edd4613a9c5ac4734bb0ad1f2210f3f3f707621e
Alan Knowles <alan@roojs.com>
2016-08-10 10:39:07 +0800

    PDO/DataObject/Introspection/mysql.php

---
+++ PDO/DataObject/Introspection/mysql.php
@@ -0,0 +1,27 @@
+<?php 
+
+class_exists('PDO_DataObject_Introspection') ? '' : require_once 'PDO/DataObject/Introspection.php';
+// move me to seperate classes...
+class PDO_DataObject_Introspection_mysql extends PDO_DataObject_Introspection
+{
+    
+    function getSpecialQuery($type)
+    {
+        switch ($type) {
+            case 'tables':
+                return 'SHOW TABLES';
+            case 'databases':
+                return 'SHOW DATABASES';
+            default:
+                return null;
+        }
+    }
+
+    // }}}
+    
+    function tableInfo($table)
+    {
+        
+    }
+    
+}
