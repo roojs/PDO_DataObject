66d5fe8eaa0977072318caa6362ce222097cc3d6
Alan Knowles <alan@roojs.com>
2016-08-17 16:57:01 +0800

    PDO/DB_DataObject.php

--- PDO/DB_DataObject.php
+++ PDO/DB_DataObject.php
@@ -1,18 +1,1 @@
-<?php
-
-/**
- *
- * Compatibility wrapper.
- *
- */
-
-class_exists('PDO_DataObject') ? '' : require_once 'PDO/DataObject.php';
-
-class DB_DataObject extends PDO_DataObject
-{
-    
-    // 
-    
-    
-    
-}
+ 
