19c97573d7d0be9328f09e2cb0ee95cd2aa6742a
Alan Knowles <alan@roojs.com>
2016-08-19 15:28:43 +0800

    PDO/DataObject/Generator/Column.php

--- PDO/DataObject/Generator/Column.php
+++ PDO/DataObject/Generator/Column.php
@@ -16,6 +16,7 @@ class PDO_DataObject_Generator_Column {
     function __construct($table,$def_ar)
     {
         $this->table = $table;
+        $this->hook = $table->hook;
         $this->name = $def_ar['name'];
         // and set other stuff?
         // put all the type parsing here!?
