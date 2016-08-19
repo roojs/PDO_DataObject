81e263f1f79405d63feec2a30e6b2a0cdd246379
Alan Knowles <alan@roojs.com>
2016-08-19 14:13:12 +0800

    PDO/DataObject/Generator/Hooks.php

--- PDO/DataObject/Generator/Hooks.php
+++ PDO/DataObject/Generator/Hooks.php
@@ -15,7 +15,23 @@ class PDO_DataObject_Generator_Hooks {
         
     }
     
-     
+      /**
+     * hook to add extra methods to all classes
+     *
+     * called once for each class, use with $this->table and
+     * $this->_definitions[$this->table], to get data out of the current table,
+     * use it to add extra methods to the default classes.
+     *
+     * @access   public
+     * @return  string added to class eg. functions.
+     */
+    function derivedHookFunctions($input = "")
+    {
+        // This is so derived generator classes can generate functions
+        // It MUST NOT be changed here!!!
+        return "";
+    }
+
     function functions($original)
     {
         return '';
@@ -24,7 +40,17 @@ class PDO_DataObject_Generator_Hooks {
     {
         return '';
     }
-    function preVar($original)
+    /**
+     * hook for var lines
+     * called each time a var line is generated, override to add extra var
+     * lines
+     *
+     * @param object t containing type,len,flags etc. from tableInfo call
+     * @param int padding number of spaces
+     * @access   public
+     * @return  string added to class eg. functions.
+     */
+    function var($original)
     {
         return '';
     }
