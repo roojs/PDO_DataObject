9715a30ed7e106cd885fb9601215e777b4b72a18
Alan Knowles <alan@roojs.com>
2016-08-19 16:14:42 +0800

    PDO/DataObject/Generator.php

--- PDO/DataObject/Generator.php
+++ PDO/DataObject/Generator.php
@@ -1325,59 +1325,7 @@ class PDO_DataObject_Generator extends PDO_DataObject
         
     }
     
-    /**
-    * Generate getter methods for class definition
-    *
-    * @param    string  $input  Existing class contents
-    * @return   string
-    * @access   public
-    */
-    function _generateGetters($input) 
-    {
-
-        $options = &PEAR::getStaticProperty('DB_DataObject','options');
-        $getters = '';
-
-        // only generate if option is set to true
-        if  (empty($options['generate_getters'])) {
-            return '';
-        }
-
-        // remove auto-generated code from input to be able to check if the method exists outside of the auto-code
-        $input = preg_replace('/(\n|\r\n)\s*###START_AUTOCODE(\n|\r\n).*(\n|\r\n)\s*###END_AUTOCODE(\n|\r\n)/s', '', $input);
-
-        $getters .= "\n\n";
-        $defs     = $this->_definitions[$this->table];
-
-        // loop through properties and create getter methods
-        foreach ($defs = $defs as $t) {
-
-            // build mehtod name
-            $methodName = 'get' . $this->getMethodNameFromColumnName($t->name);
-
-            if (!strlen(trim($t->name)) || preg_match("/function[\s]+[&]?$methodName\(/i", $input)) {
-                continue;
-            }
-
-            $getters .= "   /**\n";
-            $getters .= "    * Getter for \${$t->name}\n";
-            $getters .= "    *\n";
-            // this makes no sense - mysql multiple key (MUL) - is just for indexed columns?
-           // $getters .= (stristr($t->flags, 'multiple_key')) ? "    * @return   object\n"
-           //                                                  : "    * @return   {$t->type}\n";
-            $getters .= "    * @return   {$t->type}\n";
-            $getters .= "    * @access   public\n";
-            $getters .= "    */\n";
-            $getters .= (substr(phpversion(),0,1) > 4) ? '    public '
-                                                       : '    ';
-            $getters .= "function $methodName() {\n";
-            $getters .= "        return \$this->{$t->name};\n";
-            $getters .= "    }\n\n";
-        }
-   
-
-        return $getters;
-    }
+     
     /**
     * Generate link setter/getter methods for class definition
     *
@@ -1446,57 +1394,7 @@ class PDO_DataObject_Generator extends PDO_DataObject
          
         return $setters;
     }
-
-   /**
-    * Generate setter methods for class definition
-    *
-    * @param    string  Existing class contents
-    * @return   string
-    * @access   public
-    */
-    function _generateSetters($input) 
-    {
-
-        $options = &PEAR::getStaticProperty('DB_DataObject','options');
-        $setters = '';
-
-        // only generate if option is set to true
-        if  (empty($options['generate_setters'])) {
-            return '';
-        }
-
-        // remove auto-generated code from input to be able to check if the method exists outside of the auto-code
-        $input = preg_replace('/(\n|\r\n)\s*###START_AUTOCODE(\n|\r\n).*(\n|\r\n)\s*###END_AUTOCODE(\n|\r\n)/s', '', $input);
-
-        $setters .= "\n";
-        $defs     = $this->_definitions[$this->table];
-
-        // loop through properties and create setter methods
-        foreach ($defs = $defs as $t) {
-
-            // build mehtod name
-            $methodName = 'set' . $this->getMethodNameFromColumnName($t->name);
-
-            if (!strlen(trim($t->name)) || preg_match("/function[\s]+[&]?$methodName\(/i", $input)) {
-                continue;
-            }
-
-            $setters .= "   /**\n";
-            $setters .= "    * Setter for \${$t->name}\n";
-            $setters .= "    *\n";
-            $setters .= "    * @param    mixed   input value\n";
-            $setters .= "    * @access   public\n";
-            $setters .= "    */\n";
-            $setters .= (substr(phpversion(),0,1) > 4) ? '    public '
-                                                       : '    ';
-            $setters .= "function $methodName(\$value) {\n";
-            $setters .= "        \$this->{$t->name} = \$value;\n";
-            $setters .= "    }\n\n";
-        }
-        
-
-        return $setters;
-    }
+ 
     /**
     * Generate table Function - used when generator_no_ini is set.
     *
