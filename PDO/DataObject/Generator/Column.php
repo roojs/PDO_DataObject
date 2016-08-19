cf765acd78460d16c6c6dabe878f3aad64973f14
Alan Knowles <alan@roojs.com>
2016-08-19 15:27:22 +0800

    PDO/DataObject/Generator/Column.php

--- PDO/DataObject/Generator/Column.php
+++ PDO/DataObject/Generator/Column.php
@@ -22,7 +22,32 @@ class PDO_DataObject_Generator_Column {
      
     function toPhpVar($original)
     {
+          
+     
+        if (!strlen(trim($t->name))) {
+            return '';
+        }
+        if (!preg_match('/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/', $this->name)) {
+            echo "*****************************************************************\n".
+                 "**               WARNING COLUMN NAME UNUSABLE                  **\n".
+                 "** Found column '{$t->name}', of type  '{$t->type}'            **\n".
+                 "** Since this column name can't be converted to a php variable **\n".
+                 "** name, and the whole idea of mapping would result in a mess  **\n".
+                 "** This column has been ignored...                             **\n".
+                 "*****************************************************************\n";
+            return;
+        }
         
+        $pad = str_repeat(' ',max(2,  (30 - strlen($t->name))));
+
+        $length = empty($t->len) ? '' : '('.$t->len.')';
+        $flags = strlen($t->flags) ? (' '. trim($t->flags)) : '';
+        $body .="    {$var} \${$t->name}; {$pad}// {$t->type}{$length}{$flags}\n";
+        
+        // can not do set as PEAR::DB table info doesnt support it.
+        //if (substr($t->Type,0,3) == "set")
+        //    $sets[$t->Field] = "array".substr($t->Type,3);
+        $body .= $this->hook->varDef($t,strlen($p));
     }
     function toPhpGetter($original)
     {
