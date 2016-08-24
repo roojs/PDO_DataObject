8541d32c2e0b24fb9687f1e8ed386db7a351505a
Alan Knowles <alan@roojs.com>
2016-08-24 17:38:28 +0800

    PDO/DataObject/Introspection/mysql.php

--- PDO/DataObject/Introspection/mysql.php
+++ PDO/DataObject/Introspection/mysql.php
@@ -111,7 +111,16 @@ class PDO_DataObject_Introspection_mysql extends PDO_DataObject_Introspection
             ")
             ->fetchAllFast();
    
-        
+        if (PDO_DataObject::$debug > 3)  {
+            // this is for the test_suite...
+            $rr = array();
+            foreach($records as $r) {
+                $rr[] = json_encode($r);
+            }
+            $this->debug("\n{    " . implode( ",\n   ", $rr) ."\n}\n", 3);
+            
+            
+        }
         if (PDO_DataObject::config()['portability'] & PDO_DataObject::PORTABILITY_LOWERCASE) {
             $case_func = 'strtolower';
         } else {
