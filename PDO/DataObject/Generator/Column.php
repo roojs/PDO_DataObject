271e320007f4e8101552a5b48132d5cca6749db9
Alan Knowles <alan@roojs.com>
2016-08-26 16:06:56 +0800

    PDO/DataObject/Generator/Column.php

--- PDO/DataObject/Generator/Column.php
+++ PDO/DataObject/Generator/Column.php
@@ -567,7 +567,7 @@ class PDO_DataObject_Generator_Column
                 return '';
                     
         }
-        return "'".addslashes($this->name)."' => {$value}";
+        return var_export($this->name) . ' => ' . var_export($value);
     }
     
     
