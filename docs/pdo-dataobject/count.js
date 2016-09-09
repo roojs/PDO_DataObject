e68a7029e36706a0cd6bb06e7810f347fc479ac3
Alan Knowles <alan@roojs.com>
2016-09-09 11:02:34 +0800

    docs/pdo-dataobject/count.bjs docs/pdo-dataobject/count.js

--- docs/pdo-dataobject/count.js
+++ docs/pdo-dataobject/count.js
@@ -5,7 +5,7 @@
 count = new Roo.XComponent({
 
  _strings : {
-  '234be4dccf3be3054cb650927d03dcb0' :"t performs a select count() request on the tables key column and returns the number of resulting rows.\n The default condition applied to the count() is a combination of the object variables and whereAdd settings. \n If the constant DB_DATAOBJECT_WHEREADD_ONLY is passed in as the first parameter then only the whereAdd settings will be used.\n\n"
+  '8721587805e00c749b07cc6d25379405' :"It performs a select count() request on the tables key column and returns the number of resulting rows.\n The default condition applied to the count() is a combination of the object variables and whereAdd settings. \n If the constant DB_DATAOBJECT_WHEREADD_ONLY is passed in as the first parameter then only the whereAdd settings will be used.\n\n"
  },
 
   part     :  ["pdo-dataobject", "count" ],
@@ -60,8 +60,8 @@ count = new Roo.XComponent({
      items  : [
       {
        xtype : 'Para',
-       html : _this._strings['234be4dccf3be3054cb650927d03dcb0'] /* 
-       t performs a select count() request on the tables key column and returns the number of resulting rows.       
+       html : _this._strings['8721587805e00c749b07cc6d25379405'] /* 
+       It performs a select count() request on the tables key column and returns the number of resulting rows.       
  The default condition applied to the count() is a combination of the object variables and whereAdd settings.        
  If the constant DB_DATAOBJECT_WHEREADD_ONLY is passed in as the first parameter then only the whereAdd settings will be used.       
        
