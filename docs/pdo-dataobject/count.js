5b00433b1c1ba1675249e180714407cea7fc462c
Alan Knowles <alan@roojs.com>
2016-09-09 10:50:58 +0800

    docs/pdo-dataobject/count.bjs docs/pdo-dataobject/count.js

---
+++ docs/pdo-dataobject/count.js
@@ -0,0 +1,37 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+count = new Roo.XComponent({
+
+
+
+  part     :  ["pdo-dataobject", "count" ],
+  order    : '001-count',
+  region   : 'center',
+  parent   : false,
+  name     : "unnamed module",
+  disabled : false, 
+  permname : '', 
+  _tree : function()
+  {
+   var _this = this;
+   var MODULE = this;
+   return {
+   xtype : 'Entry',
+   name : ' count',
+   purpose : '->count() - Perform a select count() request',
+   stype : 'function',
+   xns : Roo.doc,
+   '|xns' : 'Roo.doc',
+   items  : [
+    {
+     xtype : 'Synopsis',
+     name : '$do->count',
+     returntype : 'int',
+     xns : Roo.doc,
+     '|xns' : 'Roo.doc'
+    }
+   ]
+  };  }
+});
