1d46d149cbbcb0634484ca0af152489f766f8e94
Alan Knowles <alan@roojs.com>
2016-09-20 15:09:11 +0800

    PDO/DataObject.php

--- PDO/DataObject.php
+++ PDO/DataObject.php
@@ -851,7 +851,7 @@ class PDO_DataObject
      * @access private
      */
     
-    function selectToSQL()
+    function toSelectSQL()
     {
         $quoteIdentifiers = self::$config['quote_identifiers'];
         if ($quoteIdentifiers) {
