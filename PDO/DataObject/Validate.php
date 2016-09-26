30ddac0f8fdfa8eabe8303acffd80d577dfe14c2
Alan Knowles <alan@roojs.com>
2016-09-26 15:34:40 +0800

    PDO/DataObject/Validate.php

--- PDO/DataObject/Validate.php
+++ PDO/DataObject/Validate.php
@@ -2,23 +2,31 @@
 /**
  * Validation code (simple version.)
  *
- * PHP versions 4 and 5
+ * For PHP versions  5 and 7
+ * 
+ * 
+ * Copyright (c) 2015 Alan Knowles
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU Lesser General Public License as   
+ * published by the Free Software Foundation, version 3.
  *
- * LICENSE: This source file is subject to version 3.01 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * Lesser General Lesser Public License for more details.
  *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *  
  * @category   Database
- * @package    DB_DataObject
- * @author     Alan Knowles <alan@akbkhome.com>
- * @copyright  1997-2008 The PHP Group
- * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
- * @version    CVS: $Id: Cast.php 287158 2009-08-12 13:58:31Z alan_k $
- * @link       http://pear.php.net/package/DB_DataObject
+ * @package    PDO_DataObject
+ * @author     Alan Knowles <alan@roojs.com>
+ * @copyright  2016 Alan Knowles
+ * @license    https://www.gnu.org/licenses/lgpl-3.0.en.html  LGPL 3
+ * @version    1.0
+ * @link       https://github.com/roojs/PDO_DataObject
  */
-
 // validate is needed as some constants come from it.... 
 require_once 'Validate.php';
 
