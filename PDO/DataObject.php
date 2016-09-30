7626101158591bb3663d9bdc55a8bf3440280bbf
Alan Knowles <alan@roojs.com>
2016-09-30 16:05:21 +0800

    PDO/DataObject.php

--- PDO/DataObject.php
+++ PDO/DataObject.php
@@ -1838,7 +1838,7 @@ class PDO_DataObject
             $this->select(sprintf("{$s}.{$s} as {$format}",$table,$k,$k));
         }
       
-        $this->_query['data_select'] .= "\n";
+        //$this->_query['data_select'] .= "\n";
         return $this;
     }
     /**
