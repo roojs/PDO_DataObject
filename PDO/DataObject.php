99e701c1b59744d2e847bd96d6b1a3e8e09e7121
Alan Knowles <alan@roojs.com>
2016-10-05 18:03:34 +0800

    PDO/DataObject.php

--- PDO/DataObject.php
+++ PDO/DataObject.php
@@ -4389,7 +4389,6 @@ class PDO_DataObject
             
             // we ignore include here... - as
              
-            // this is borked ... for multiple jions..
             $this->joinAdd($xx, 'LEFT', 'join_'.$ocl.'_'. $col, $ocl);
             
             if (!empty($cfg['exclude']) && in_array($ocl, $cfg['exclude'])) {
