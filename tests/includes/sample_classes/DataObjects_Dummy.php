9e001e1d9009b513dad15f80bfd2b8293929246c
Alan Knowles <alan@roojs.com>
2016-10-06 13:02:57 +0800

    tests/includes/sample_classes/DataObjects_Dummy.php

---
+++ tests/includes/sample_classes/DataObjects_Dummy.php
@@ -0,0 +1,12 @@
+<?php
+
+// a simple class for customers..
+
+class DataObjects_Dummy extends PDO_DataObject {
+    
+    var $__table = 'Dummy';
+    
+    var $ex_null_int;
+    var $ex_null_string;
+    
+}
