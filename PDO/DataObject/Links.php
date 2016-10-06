076a59b1d8170e0d1571634e0b27d64ee02924ef
Alan Knowles <alan@roojs.com>
2016-10-06 10:52:55 +0800

    PDO/DataObject/Links.php

--- PDO/DataObject/Links.php
+++ PDO/DataObject/Links.php
@@ -309,6 +309,7 @@ class PDO_DataObject_Links
         }
         // otherwise it's a set call..
 
+        // we used to support array as args.. ?? why!?!
         $assign = is_array($args) ? $args[0] : $args;
          
 
