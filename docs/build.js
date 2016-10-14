8d60ae0995622499a980e934a0d9ac15cdb310e8
Alan Knowles <alan@roojs.com>
2016-10-14 15:28:15 +0800

    docs/build.bjs docs/build.js

--- docs/build.js
+++ docs/build.js
@@ -5,9 +5,9 @@
 build = new Roo.XComponent({
 
  _strings : {
+  '7dd6d86a2c8648fe541335db76a0cabb' :"<p>\nThere are two ways that queries are built in PDO_DataObjects\n</p>\n<ul>\n<li>Properties of the object, that match the column names in the table\n<li>Calling the query modification methods.\n</ul>\n\n<p>\nBy just assigning values to the DataObject, then running fetchAll() or find/fetch() the query will be built \nbased on the values you set.\n</p>\n\n",
   '6ed348e04674567827e341bb5b6d9d82' :"Example of using PDO_DataObjects",
   '8413aa2e38fdd078a3d96e34592d286a' :"Building Queries",
-  'cca65c5b03db8ac0d31402bef3188f65' :"<p>\nPDO_DataObject is a SQL Builder and Data Modeling Layer built on top of PHP's PDO library. Its main purpose is to\n</p>\n<ul>\n<li>Build SQL and execute statements based on the objects variables.\n<li>Group source code around the data that they relate to.\n<li>Provide a simple consistent API to access and manipulate that data.\n</ul>\n\n<p>\nPDO_DataObject is based on PEAR's <a href=\"https://pear.php.net/manual/en/package.database.db-dataobject.php\">DB_DataObject</a> and for most purposes is \nfunctionally compatibly (see <a href=\"db-dataobject-compatibility\">DB_DataObject Compatibility</a>)\n</p>\n\n<p>\nSo what does that mean in English? Well, if you look around at some of the better written PHP applications and frameworks\n out there, you will notice a common approach to using classes to wrap access to database tables or groups.\n</p>\n<p>\nPDO_DataObjects follows this pattern, In normal usage, you define one Class for each table that you use \n(you can use the genreator tools to automate this process), \n\nonce you have created the classes, and configured PDO_DataObjects you can the access the database  like this\n\n",
   'd6aa8be6ff38aa217305484e5dd38a88' :"<p>\nWhat that code does should be reasonably clear\n</p>\n<ul>\n<li>Load, and create an instance of the 'events' class\n<li>fetch the record with the primary ID = 3523\n<li>set the value of 'action' to 'testing'\n<li>perform a database update\n</ul>\n\n<p>\nMost methods in PDO_DataObjects support chaining, except on methods which are designed to be\n compatibile with DB_DataObjects.\n</p>\n\n\n"
  },
 
@@ -36,30 +36,19 @@ build = new Roo.XComponent({
     },
     {
      xtype : 'Element',
-     html : _this._strings['cca65c5b03db8ac0d31402bef3188f65'] /* 
+     html : _this._strings['7dd6d86a2c8648fe541335db76a0cabb'] /* 
      <p>     
-PDO_DataObject is a SQL Builder and Data Modeling Layer built on top of PHP's PDO library. Its main purpose is to     
+There are two ways that queries are built in PDO_DataObjects     
 </p>     
 <ul>     
-<li>Build SQL and execute statements based on the objects variables.     
-<li>Group source code around the data that they relate to.     
-<li>Provide a simple consistent API to access and manipulate that data.     
+<li>Properties of the object, that match the column names in the table     
+<li>Calling the query modification methods.     
 </ul>     
      
 <p>     
-PDO_DataObject is based on PEAR's <a href="https://pear.php.net/manual/en/package.database.db-dataobject.php">DB_DataObject</a> and for most purposes is      
-functionally compatibly (see <a href="db-dataobject-compatibility">DB_DataObject Compatibility</a>)     
+By just assigning values to the DataObject, then running fetchAll() or find/fetch() the query will be built      
+based on the values you set.     
 </p>     
-     
-<p>     
-So what does that mean in English? Well, if you look around at some of the better written PHP applications and frameworks     
- out there, you will notice a common approach to using classes to wrap access to database tables or groups.     
-</p>     
-<p>     
-PDO_DataObjects follows this pattern, In normal usage, you define one Class for each table that you use      
-(you can use the genreator tools to automate this process),      
-     
-once you have created the classes, and configured PDO_DataObjects you can the access the database  like this     
      
 
      */ ,
