3a3c8d22d662cacfc7d94bfd439355195c7d3dfc
Alan Knowles <alan@roojs.com>
2016-10-14 16:00:25 +0800

    PDO/DataObject.php

--- PDO/DataObject.php
+++ PDO/DataObject.php
@@ -4679,7 +4679,7 @@ class PDO_DataObject
      * 
      * @see #autoJoin for more details.
      *
-     * @call join
+     * @category join
      * @param     array     Configuration
      *          exclude  Array of columns to exclude from results (eg. modified_by_id)
      *                    Use TABLENAME.* to prevent a join occuring to a specific table.
