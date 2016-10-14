21cf50fcd84f67ce4993cfb6c431602417a26771
Alan Knowles <alan@roojs.com>
2016-10-14 17:38:45 +0800

    PDO/DataObject.php

--- PDO/DataObject.php
+++ PDO/DataObject.php
@@ -5148,6 +5148,7 @@ class PDO_DataObject
  
     /**
      * Gets the DB result object related to the objects active query
+     * 
      * @category result
      * @access public
      * @return PDOStatement|false  
