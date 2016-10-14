ede262e48d97b07953a2e477051e3a3c123b3fb7
Alan Knowles <alan@roojs.com>
2016-10-14 11:27:11 +0800

    docs/pdo-dataobject/index.bjs docs/pdo-dataobject/index.js

--- docs/pdo-dataobject/index.js
+++ docs/pdo-dataobject/index.js
@@ -7,14 +7,13 @@ index = new Roo.XComponent({
  _strings : {
   '150e558f4884668e5a5b45a8410b5d2f' :"Introspection - Database and table schema information",
   'aca0253c82fea77fa2c8cbe60cd619f2' :"Joins and referenced Tables",
-  '098f6bcd4621d373cade4e832627b4f6' :"test",
   'c2b942b501dc222d608980c0ed40b07c' :"Fetching Results",
   '0b79795d3efc95b9976c7c5b933afce2' :"Introduction",
   '556315ce9fbee2dfda70f8a91138d2b3' :"Create, Update and Delete",
   '5e434159c315edc6ae8d816255bffaec' :"Debugging & Error Handling",
   '2c918a06bae30fdcf78f8810894a67af' :"Working with Results",
-  '254f642527b45bc260048e30704edb39' :"Configuration",
-  '8413aa2e38fdd078a3d96e34592d286a' :"Building Queries"
+  '8413aa2e38fdd078a3d96e34592d286a' :"Building Queries",
+  '254f642527b45bc260048e30704edb39' :"Configuration"
  },
 
   part     :  ["pdo-dataobject", "index" ],
@@ -137,23 +136,7 @@ index = new Roo.XComponent({
                name : 'introduction',
                title : _this._strings['0b79795d3efc95b9976c7c5b933afce2'] /* Introduction */,
                xns : Roo.doc,
-               '|xns' : 'Roo.doc',
-               items  : [
-                {
-                 xtype : 'NavGroup',
-                 xns : Roo.bootstrap,
-                 '|xns' : 'Roo.bootstrap',
-                 items  : [
-                  {
-                   xtype : 'NavItem',
-                   href : 'test',
-                   html : _this._strings['098f6bcd4621d373cade4e832627b4f6'] /* test */,
-                   xns : Roo.bootstrap,
-                   '|xns' : 'Roo.bootstrap'
-                  }
-                 ]
-                }
-               ]
+               '|xns' : 'Roo.doc'
               },
               {
                xtype : 'NavCategory',
