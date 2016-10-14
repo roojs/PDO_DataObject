29dfecd73dc259b7b89224fcaa856c5ca50f3562
Alan Knowles <alan@roojs.com>
2016-10-14 16:07:19 +0800

    docs/join.bjs docs/join.js

--- docs/join.js
+++ docs/join.js
@@ -7,8 +7,8 @@ join = new Roo.XComponent({
  _strings : {
   'aca0253c82fea77fa2c8cbe60cd619f2' :"Joins and referenced Tables",
   'e663c4f830fd6401ebec75faaab19a65' :"Example of a links.ini with one table",
-  '09ab75b637f08b93be3b8a05dbd2da9c' :"<p>\nIn the above example\n</p>\n<ul>\n<li>The column company_id points to the table company using column id\n<li>The column photo_id points to the table images using the column id.\n</ul>\n\n<p>\nUsing JoinAll would modify the select query to join both these tables in the query, and select the relivant information\n</p>\n\n\n",
-  '15bcef4620646f469603a75168da6b10' :"<p>\nWhen designing a database, often some tables are related to others - a membership table would\n contain a reference to a person's id and the group id that they are a member of. \n</p> \n<p>\nUsing the Join and Link methods, you can fetch related information easily.\n</p>\n\n\n<p>\nThe relationship information is supported using by a \"links.ini\" file. which stores the \nrelations ship between tables, maping one tables column to anothers.\n</p>\n\n<p>This \"links.ini\" file is used by the <a href=\"#pdo-dataobject/link\">link</a>,\n<a href=\"#pdo-dataobject/joinAll\">joinAll</a>, <a href=\"#pdo-dataobject/join\">join</a> and\n<a href=\"#pdo-dataobject/joinAdd\">joinAdd</a> methods\n</p>\n\n<p>Used with methods like <a href=\"#pdo-dataobject/selectAs\">selectAs</a> it is possible to very quickly\nbuild complex requests and fetch data from the database without having to type long queries.\n</p>\n"
+  '15bcef4620646f469603a75168da6b10' :"<p>\nWhen designing a database, often some tables are related to others - a membership table would\n contain a reference to a person's id and the group id that they are a member of. \n</p> \n<p>\nUsing the Join and Link methods, you can fetch related information easily.\n</p>\n\n\n<p>\nThe relationship information is supported using by a \"links.ini\" file. which stores the \nrelations ship between tables, maping one tables column to anothers.\n</p>\n\n<p>This \"links.ini\" file is used by the <a href=\"#pdo-dataobject/link\">link</a>,\n<a href=\"#pdo-dataobject/joinAll\">joinAll</a>, <a href=\"#pdo-dataobject/join\">join</a> and\n<a href=\"#pdo-dataobject/joinAdd\">joinAdd</a> methods\n</p>\n\n<p>Used with methods like <a href=\"#pdo-dataobject/selectAs\">selectAs</a> it is possible to very quickly\nbuild complex requests and fetch data from the database without having to type long queries.\n</p>\n",
+  'a4882f671a5210f8175ffb683cdfe878' :"<p>\nIn the above example\n</p>\n<ul>\n<li>The column company_id points to the table company using column id\n<li>The column photo_id points to the table images using the column id.\n</ul>\n\n<p>\nUsing <a href=\"#pdo-dataobject/joinAll\">joinAll</a>, would modify the select query to join both these tables in the query, and select the relivant information\n</p>\n\n\n"
  },
 
   part     :  ["docs", "join" ],
@@ -88,7 +88,7 @@ build complex requests and fetch data from the database without having to type l
     },
     {
      xtype : 'Element',
-     html : _this._strings['09ab75b637f08b93be3b8a05dbd2da9c'] /* 
+     html : _this._strings['a4882f671a5210f8175ffb683cdfe878'] /* 
      <p>     
 In the above example     
 </p>     
@@ -98,7 +98,7 @@ In the above example
 </ul>     
      
 <p>     
-Using JoinAll would modify the select query to join both these tables in the query, and select the relivant information     
+Using <a href="#pdo-dataobject/joinAll">joinAll</a>, would modify the select query to join both these tables in the query, and select the relivant information     
 </p>     
      
      
