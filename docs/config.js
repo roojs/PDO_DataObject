c12aa8e78bb6924a6a906d914519905b369cd8c6
Alan Knowles <alan@roojs.com>
2016-10-14 14:50:30 +0800

    docs/config.bjs docs/config.js

--- docs/config.js
+++ docs/config.js
@@ -100,6 +100,28 @@ XXXX=1|YYY=2
        ]
       }
      ]
+    },
+    {
+     xtype : 'Entry',
+     xns : Roo.doc,
+     '|xns' : 'Roo.doc',
+     items  : [
+      {
+       xtype : 'Section',
+       lang : 'php',
+       xns : Roo.doc,
+       '|xns' : 'Roo.doc',
+       items  : [
+        {
+         xtype : 'Example',
+         code : 'PDO_DataObject::config(\'database\', \'mysql://username:test@localhost:3344/somedb#\' . implode( \'|\',\n    array(\n        \'MYSQL_ATTR_SSL_KEY=/path/to/client-key.pem\',\n        \'MYSQL_ATTR_SSL_CERT=/path/to/client-cert.pem\',\n        \'MYSQL_ATTR_SSL_CA=/path/to/ca-cert.pem\',\n        \n        \'MYSQL_ATTR_LOCAL_INFILE=1\',\n        \'MYSQL_ATTR_INIT_COMMAND=2\',\n       // \'MYSQL_ATTR_READ_DEFAULT_FILE=3\',\n       //  \'MYSQL_ATTR_READ_DEFAULT_GROUP=4\',\n        \'MYSQL_ATTR_MAX_BUFFER_SIZE=5\',\n        \'MYSQL_ATTR_DIRECT_QUERY=6\',\n        \'MYSQL_ATTR_FOUND_ROWS=7\',\n        \'MYSQL_ATTR_IGNORE_SPACE=8\',\n        \'MYSQL_ATTR_COMPRESS=9\',\n        \'MYSQL_ATTR_SSL_CIPHER=10\',\n        \'MYSQL_ATTR_SSL_KEY=11\',\n        \'MYSQL_ATTR_MULTI_STATEMENTS=12\',\n        \n    )));\n(new PDO_DataObject())->PDO();\n',
+         title : _this._strings['35be5669049f8fb369c7654567b1d1ab'] /* Mysql Connection examples */,
+         xns : Roo.doc,
+         '|xns' : 'Roo.doc'
+        }
+       ]
+      }
+     ]
     }
    ]
   };  }
