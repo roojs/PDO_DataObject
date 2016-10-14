eae667be22c3bcf32633deff69fe69f8fe53dd9e
Alan Knowles <alan@roojs.com>
2016-10-14 10:21:52 +0800

    docs/pdo-dataobject/index.bjs docs/pdo-dataobject/index.js

--- docs/pdo-dataobject/index.js
+++ docs/pdo-dataobject/index.js
@@ -73,6 +73,34 @@ index = new Roo.XComponent({
                title : _this._strings['0b79795d3efc95b9976c7c5b933afce2'] /* Introduction */,
                xns : Roo.doc,
                '|xns' : 'Roo.doc'
+              },
+              {
+               xtype : 'NavCategory',
+               name : 'introduction',
+               title : _this._strings['0b79795d3efc95b9976c7c5b933afce2'] /* Introduction */,
+               xns : Roo.doc,
+               '|xns' : 'Roo.doc'
+              },
+              {
+               xtype : 'NavCategory',
+               name : 'introduction',
+               title : _this._strings['0b79795d3efc95b9976c7c5b933afce2'] /* Introduction */,
+               xns : Roo.doc,
+               '|xns' : 'Roo.doc'
+              },
+              {
+               xtype : 'NavCategory',
+               name : 'introduction',
+               title : _this._strings['0b79795d3efc95b9976c7c5b933afce2'] /* Introduction */,
+               xns : Roo.doc,
+               '|xns' : 'Roo.doc'
+              },
+              {
+               xtype : 'NavCategory',
+               name : 'introduction',
+               title : _this._strings['0b79795d3efc95b9976c7c5b933afce2'] /* Introduction */,
+               xns : Roo.doc,
+               '|xns' : 'Roo.doc'
               }
              ]
             }
