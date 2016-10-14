dcc9acfc2e471e4040e3b14c0aafc3170205d3d9
Alan Knowles <alan@roojs.com>
2016-10-14 16:18:50 +0800

    docs/crud.bjs docs/crud.js

--- docs/crud.js
+++ docs/crud.js
@@ -7,8 +7,7 @@ crud = new Roo.XComponent({
  _strings : {
   '6ed348e04674567827e341bb5b6d9d82' :"Example of using PDO_DataObjects",
   '390621fb37678d91d0ccd13331d8e65e' :"<p>\nObviously one of the main reasons to use a database is to change the data contained in it.\n PDO_DataObjects has a number of was of doing this. \n</p>\n<ul>\n<li><a href=\"#pdo-dataobjects/insert\">insert</a> a record into the database, using the properties of the object\n<li><a href=\"#pdo-dataobjects/update\">update</a> update the record with some changes.\n<li><a href=\"#pdo-dataobjects/delete\">delete</a>delete a record  - usually based on the primary id\n<ul>\n\n<p>\nIn PDO_DataObject these methods have been added to, allowing chained modifications\n\n<ul>\n<li><a href=\"#pdo-dataobjects/load\">load</a> is a chainable method to load data \n<li><a href=\"#pdo-dataobjects/snapshot\">snapshot</a> keeps a record of the objects properties at the beginning of the changes\n<li><a href=\"#pdo-dataobjects/snapshot\">save</a> either inserts or updates the record, if an update is done, then it will \nuse the snapshot information to determine what to update.\n</li>\n</ul>\n\n",
-  '0b2e73391e1393962ec1b53fc13a31da' :"Create, Update and Deleting records",
-  'd6aa8be6ff38aa217305484e5dd38a88' :"<p>\nWhat that code does should be reasonably clear\n</p>\n<ul>\n<li>Load, and create an instance of the 'events' class\n<li>fetch the record with the primary ID = 3523\n<li>set the value of 'action' to 'testing'\n<li>perform a database update\n</ul>\n\n<p>\nMost methods in PDO_DataObjects support chaining, except on methods which are designed to be\n compatibile with DB_DataObjects.\n</p>\n\n\n"
+  '0b2e73391e1393962ec1b53fc13a31da' :"Create, Update and Deleting records"
  },
 
   part     :  ["docs", "crud" ],
@@ -84,30 +83,6 @@ use the snapshot information to determine what to update.
        ]
       }
      ]
-    },
-    {
-     xtype : 'Element',
-     html : _this._strings['d6aa8be6ff38aa217305484e5dd38a88'] /* 
-     <p>     
-What that code does should be reasonably clear     
-</p>     
-<ul>     
-<li>Load, and create an instance of the 'events' class     
-<li>fetch the record with the primary ID = 3523     
-<li>set the value of 'action' to 'testing'     
-<li>perform a database update     
-</ul>     
-     
-<p>     
-Most methods in PDO_DataObjects support chaining, except on methods which are designed to be     
- compatibile with DB_DataObjects.     
-</p>     
-     
-     
-
-     */ ,
-     xns : Roo.bootstrap,
-     '|xns' : 'Roo.bootstrap'
     }
    ]
   };  }
