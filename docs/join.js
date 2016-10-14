170a3d65b9c11b881e24ff4d757be99a84d18111
Alan Knowles <alan@roojs.com>
2016-10-14 16:04:44 +0800

    docs/join.bjs docs/join.js

--- docs/join.js
+++ docs/join.js
@@ -6,7 +6,7 @@ join = new Roo.XComponent({
 
  _strings : {
   'aca0253c82fea77fa2c8cbe60cd619f2' :"Joins and referenced Tables",
-  '6ed348e04674567827e341bb5b6d9d82' :"Example of using PDO_DataObjects",
+  'e663c4f830fd6401ebec75faaab19a65' :"Example of a links.ini with one table",
   '15bcef4620646f469603a75168da6b10' :"<p>\nWhen designing a database, often some tables are related to others - a membership table would\n contain a reference to a person's id and the group id that they are a member of. \n</p> \n<p>\nUsing the Join and Link methods, you can fetch related information easily.\n</p>\n\n\n<p>\nThe relationship information is supported using by a \"links.ini\" file. which stores the \nrelations ship between tables, maping one tables column to anothers.\n</p>\n\n<p>This \"links.ini\" file is used by the <a href=\"#pdo-dataobject/link\">link</a>,\n<a href=\"#pdo-dataobject/joinAll\">joinAll</a>, <a href=\"#pdo-dataobject/join\">join</a> and\n<a href=\"#pdo-dataobject/joinAdd\">joinAdd</a> methods\n</p>\n\n<p>Used with methods like <a href=\"#pdo-dataobject/selectAs\">selectAs</a> it is possible to very quickly\nbuild complex requests and fetch data from the database without having to type long queries.\n</p>\n",
   'd6aa8be6ff38aa217305484e5dd38a88' :"<p>\nWhat that code does should be reasonably clear\n</p>\n<ul>\n<li>Load, and create an instance of the 'events' class\n<li>fetch the record with the primary ID = 3523\n<li>set the value of 'action' to 'testing'\n<li>perform a database update\n</ul>\n\n<p>\nMost methods in PDO_DataObjects support chaining, except on methods which are designed to be\n compatibile with DB_DataObjects.\n</p>\n\n\n"
  },
@@ -77,8 +77,8 @@ build complex requests and fetch data from the database without having to type l
        items  : [
         {
          xtype : 'Example',
-         code : 'person]\ncompany_id = company:id\nphoto_id = images:id\n\n\n',
-         title : _this._strings['6ed348e04674567827e341bb5b6d9d82'] /* Example of using PDO_DataObjects */,
+         code : '[person]\ncompany_id = company:id\nphoto_id = images:id\n\n\n',
+         title : _this._strings['e663c4f830fd6401ebec75faaab19a65'] /* Example of a links.ini with one table */,
          xns : Roo.doc,
          '|xns' : 'Roo.doc'
         }
