712edae0e32eb50ac19150d276b81a67c0facb2d
Alan Knowles <alan@roojs.com>
2016-10-14 14:16:55 +0800

    docs/creating-dataobjects.bjs docs/creating-dataobjects.js

--- docs/creating-dataobjects.js
+++ docs/creating-dataobjects.js
@@ -8,7 +8,8 @@ creating-dataobjects = new Roo.XComponent({
   '2ee868dc71d76b74e5af6c689ef9ffe0' :"Creating DataObjects",
   '2314287293fbe38854242eac7f34855a' :"<p>\nThis will result in the creation of an 'ini' file containing the database structure.\n</p>\n",
   'd06bda29ade3b12c79092ea8e173dbc0' :"Creating schema and Base DataObjects",
-  'd438a0af0c2a3eb34111e45e615fe4a7' :"<p>\nThere are two components that are needed before you can start using DataObjects in your code\n\n<ul>\n<li>a schema file describing the database\n<li>PHP classes representing each of the tables\n</ul>\n<p>\nBoth of these can be created using the tools included with PDO_DataObjects, or you can use the code in \nthe Generator to write the schema files when a page is requested, and cache the results.\n\n<p>\nFor basic usage, use the file PDO/DataObject/createTables.php to create your schema file\n "
+  'd438a0af0c2a3eb34111e45e615fe4a7' :"<p>\nThere are two components that are needed before you can start using DataObjects in your code\n\n<ul>\n<li>a schema file describing the database\n<li>PHP classes representing each of the tables\n</ul>\n<p>\nBoth of these can be created using the tools included with PDO_DataObjects, or you can use the code in \nthe Generator to write the schema files when a page is requested, and cache the results.\n\n<p>\nFor basic usage, use the file PDO/DataObject/createTables.php to create your schema file\n ",
+  '765ecf0efc7aa64d38c808cb91d33077' :"Example in file"
  },
 
   part     :  ["docs", "creating-dataobjects" ],
@@ -102,7 +103,7 @@ This will result in the creation of an 'ini' file containing the database struct
         {
          xtype : 'Example',
          code : '#/usr/bin/php  PDO/DataObjects/createTables.php  your_ini_file.ini',
-         title : _this._strings['d06bda29ade3b12c79092ea8e173dbc0'] /* Creating schema and Base DataObjects */,
+         title : _this._strings['765ecf0efc7aa64d38c808cb91d33077'] /* Example in file */,
          xns : Roo.doc,
          '|xns' : 'Roo.doc'
         }
