0f44c10e0455357e65c733a9910e18d9204dcb41
Alan Knowles <alan@roojs.com>
2016-10-14 12:11:33 +0800

    docs/introduction.bjs docs/introduction.js

--- docs/introduction.js
+++ docs/introduction.js
@@ -5,6 +5,7 @@
 introduction = new Roo.XComponent({
 
  _strings : {
+  '8283bcdf57e3f7e4473e8002b5a240dd' :"DB_DataObject is a SQL Builder and Data Modeling Layer built on top of PEAR::DB. Its main purpose is to\n\nBuild SQL and execute statements based on the objects variables.\nGroup source code around the data that they relate to.\nProvide a simple consistent API to access and manipulate that data.\nSo what does that mean in English? Well, if you look around at some of the better written PHP applications and frameworks out there, you will notice a common approach to using classes to wrap access to database tables or groups. The prime example of this is the person object, which would frequently look something like this.\n\n",
   '0b79795d3efc95b9976c7c5b933afce2' :"Introduction"
  },
 
@@ -33,6 +34,16 @@ introduction = new Roo.XComponent({
     },
     {
      xtype : 'Container',
+     html : _this._strings['8283bcdf57e3f7e4473e8002b5a240dd'] /* 
+     DB_DataObject is a SQL Builder and Data Modeling Layer built on top of PEAR::DB. Its main purpose is to     
+     
+Build SQL and execute statements based on the objects variables.     
+Group source code around the data that they relate to.     
+Provide a simple consistent API to access and manipulate that data.     
+So what does that mean in English? Well, if you look around at some of the better written PHP applications and frameworks out there, you will notice a common approach to using classes to wrap access to database tables or groups. The prime example of this is the person object, which would frequently look something like this.     
+     
+
+     */ ,
      xns : Roo.bootstrap,
      '|xns' : 'Roo.bootstrap'
     }
