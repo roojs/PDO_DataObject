92a773912eb204e6d30803852c2abbec6c0d00db
Alan Knowles <alan@roojs.com>
2016-10-14 15:49:32 +0800

    docs/fetch.bjs docs/fetch.js

--- docs/fetch.js
+++ docs/fetch.js
@@ -7,7 +7,7 @@ fetch = new Roo.XComponent({
  _strings : {
   'c2b942b501dc222d608980c0ed40b07c' :"Fetching Results",
   'f42214f12f8d7e37f19c860651f81d31' :"<p>\nTo actually fetch data from the database, one of the fetch commands must be run.\n</p>\n\n<p>\nFor loading single rows\n</p>\n<ul>\n<li><a href=\"#pdo-dataobject/load\">load</a> - this loads a single row (Chainable)\n<li><a href=\"#pdo-dataobject/reload\">reload</a> - this reloads the existing object from the database (Chainable)\n<li><a href=\"#pdo-dataobject/get\">get</a> - this loads a single row\n<li><a href=\"#pdo-dataobject/count\">count</a> - counts the number of a results a query will return.\n</ul>\n\n\n<p>\nFor sending queries\n</p>\n<ul>\n<li><a href=\"#pdo-dataobject/find\">find</a> - builds the query from the properties and called methods\n<li><a href=\"#pdo-dataobject/query\">query</a> - sends a raw query\n<li><a href=\"#pdo-dataobject/get\">get</a> - this loads a single row\n</ul>\n\n<p>\nFor fetching results\n</p>\n<ul>\n<li><a href=\"#pdo-dataobject/fetch\">fetch</a> - loads the object's propeties with the values from the database\n<li><a href=\"#pdo-dataobject/query\">fetchAll</a> - retrieves all the results in various formats\n<li><a href=\"#pdo-dataobject/get\">fetchAllAssoc</a> - very fast way to retrieve multiple rows of data.\n</ul>\n\n\n<h3>Buffered results.</h3>\n<p>\nNormally results are buffered, and find() will return the number of rows returned, however if you are fetching \na unbuffered, find() will return true, even if there are no results.\n\n</p>",
-  '6ed348e04674567827e341bb5b6d9d82' :"Example of using PDO_DataObjects",
+  'aed714b47bb8cb9c375ef026e478f1b4' :"Fetching with buffering turned off",
   'd6aa8be6ff38aa217305484e5dd38a88' :"<p>\nWhat that code does should be reasonably clear\n</p>\n<ul>\n<li>Load, and create an instance of the 'events' class\n<li>fetch the record with the primary ID = 3523\n<li>set the value of 'action' to 'testing'\n<li>perform a database update\n</ul>\n\n<p>\nMost methods in PDO_DataObjects support chaining, except on methods which are designed to be\n compatibile with DB_DataObjects.\n</p>\n\n\n"
  },
 
@@ -95,7 +95,7 @@ a unbuffered, find() will return true, even if there are no results.
         {
          xtype : 'Example',
          code : 'PDO_DataObject::factory(\'Events\')\n    ->load(3523)\n    ->set([\'action\' => \"testing\" ])\n    ->save();\n',
-         title : _this._strings['6ed348e04674567827e341bb5b6d9d82'] /* Example of using PDO_DataObjects */,
+         title : _this._strings['aed714b47bb8cb9c375ef026e478f1b4'] /* Fetching with buffering turned off */,
          xns : Roo.doc,
          '|xns' : 'Roo.doc'
         }
