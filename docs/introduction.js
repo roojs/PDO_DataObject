8f63931eb3b203472d6cc6161c6461524e6aa38d
Alan Knowles <alan@roojs.com>
2016-10-14 12:32:49 +0800

    docs/introduction.bjs docs/introduction.js

--- docs/introduction.js
+++ docs/introduction.js
@@ -58,6 +58,30 @@ So what does that mean in English? Well, if you look around at some of the bette
      '|xns' : 'Roo.bootstrap'
     },
     {
+     xtype : 'Element',
+     html : _this._strings['0bc697cb5fdd2bcbc55179802b8160cd'] /* 
+     <p>     
+PDO_DataObject is a SQL Builder and Data Modeling Layer built on top of PHP's PDO library. Its main purpose is to     
+</p>     
+<ul>     
+<li>Build SQL and execute statements based on the objects variables.     
+<li>Group source code around the data that they relate to.     
+<li>Provide a simple consistent API to access and manipulate that data.     
+</ul>     
+     
+<p>     
+PDO_DataObject is based on PEAR's <a href="https://pear.php.net/manual/en/package.database.db-dataobject.php">DB_DataObject</a> and for most purposes is      
+functionally compatibly (see <a href="db-dataobject-compatibility">DB_DataObject Compatibility</a>)     
+</p>     
+     
+So what does that mean in English? Well, if you look around at some of the better written PHP applications and frameworks out there, you will notice a common approach to using classes to wrap access to database tables or groups. The prime example of this is the person object, which would frequently look something like this.     
+     
+
+     */ ,
+     xns : Roo.bootstrap,
+     '|xns' : 'Roo.bootstrap'
+    },
+    {
      xtype : 'Entry',
      xns : Roo.doc,
      '|xns' : 'Roo.doc',
