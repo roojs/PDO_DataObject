b9ba07741afeb76035ef4ba1e719e2fd9dbeaaba
Alan Knowles <alan@roojs.com>
2016-10-14 11:53:33 +0800

    docs/introduction.bjs docs/introduction.js

---
+++ docs/introduction.js
@@ -0,0 +1,25 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+introduction = new Roo.XComponent({
+
+
+
+  part     :  ["docs", "introduction" ],
+  order    : '001-introduction',
+  region   : 'center',
+  parent   : false,
+  name     : "unnamed module",
+  disabled : false, 
+  permname : '', 
+  _tree : function()
+  {
+   var _this = this;
+   var MODULE = this;
+   return {
+   xtype : 'Body',
+   xns : Roo.bootstrap,
+   '|xns' : 'Roo.bootstrap'
+  };  }
+});
