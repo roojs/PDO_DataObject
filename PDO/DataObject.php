8e737e4388e8a04fc301a6bd52e5694b0cba9dec
edward <edward@roojs.com>
2017-10-10 20:15:41 +0800

    PDO/DataObject.php

--- PDO/DataObject.php
+++ PDO/DataObject.php
@@ -2904,8 +2904,6 @@ class PDO_DataObject
         $where = !empty($this->_query) && !empty($this->_query['condition']) ?
             $this->_query['condition'] : '';
         
-        print_R($where);exit;
-        
         if (!$useWhere) {
 
             $keys = $this->keys();
