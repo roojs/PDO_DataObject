1ee92c453f276de53b233cd3555feb8cc65255c9
Alan Knowles <alan@roojs.com>
2021-04-28 12:29:07 +0800

    PDO/DataObject.php

--- PDO/DataObject.php
+++ PDO/DataObject.php
@@ -1594,8 +1594,9 @@ class PDO_DataObject
 
         if ($this->_result === false) {
             if (!$kcl && $k !== false &&  $this->_query['data_select'] === false) {
-                
-                $this->select($k == PDO_DataObject::FETCH_FAST ? null : $k);
+                if ($k !== PDO_DataObject::FETCH_FAST) {
+                    $this->select($k);
+                }
                 if ($v !== false) {
                     $this->select($v);
                 }
