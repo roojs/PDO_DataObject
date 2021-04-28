394175097c9c5e2a9540b8e78e0ac3c10ceafc52
Alan Knowles <alan@roojs.com>
2021-04-28 12:12:56 +0800

    PDO/DataObject.php

--- PDO/DataObject.php
+++ PDO/DataObject.php
@@ -1633,6 +1633,7 @@ class PDO_DataObject
                 while($row = $this->_result->fetch(PDO::FETCH_ASSOC)) {
                     $ret[] =  $row[$k];
                 }
+                die("here");
                 return $ret;
             
             // key value
