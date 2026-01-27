2c02672adcbb3d9ae6a68d94bddcf8322d00f746
Andrea Kotkova <andrea.kotkova@centralnic.com>
2026-01-27 17:48:16 +0100

    Fix: hide sensitive data

--- PDO/DataObject.php
+++ PDO/DataObject.php
@@ -585,8 +585,12 @@ class PDO_DataObject
 
             default:
                 // by default we need to validate a little bit..
-                if (empty($dsn_ar['host']) || empty($dsn_ar['path'])  || strlen($dsn_ar['path']) < 2) {
-                    return $this->raise("Invalid syntax of DSN : {$dsn}\n". print_r($dsn_ar,true), self::ERROR_INVALIDCONFIG);
+				if (empty($dsn_ar['host'])) {
+					return $this->raise("Invalid syntax of DSN : host is not specified", self::ERROR_INVALIDCONFIG);
+				}
+
+                if (empty($dsn_ar['path'])  || strlen($dsn_ar['path']) < 2) {
+                    return $this->raise("Invalid syntax of DSN : path is not specified", self::ERROR_INVALIDCONFIG);
                 }
                 $pdo_dsn =
                     $dsn_ar['scheme'] . ':' .
